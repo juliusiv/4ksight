d9577a586f0358975cf04ec85bd3d85bb94ca826
Dimitri Pantelli <dimitri.pantelli@gmail.com>
2022-01-29 17:45:41 +0000

    Fixed query selectors

--- content.js
+++ content.js
@@ -1,8 +1,20 @@
-var REVIEW_SELECTOR = "#result-albumreviews ul li.result-item";
-var ALBUM_LINK_SELECTOR = "a.album-link";
+if (location.pathname.startsWith("/artists")) {
+  var REVIEW_SELECTOR = "#result-albumreviews ul li.result-item";
+  var ALBUM_LINK_SELECTOR = "a.review__link";
+} else if (location.pathname.startsWith("/reviews")) {
+  var REVIEW_SELECTOR = ".fragment-list > .review";
+  var ALBUM_LINK_SELECTOR = "a.review__link";
+} else if (location.pathname.startsWith("/best")) {
+  var REVIEW_SELECTOR =
+    "#best-new-overview .bnm-album-hero, #best-new-albums ul.square-list > li, #best-new-reissues ul.square-list > li";
+  var ALBUM_LINK_SELECTOR = ".bnm-hero__link-block, .link-block";
+} else if (location.pathname == '/') {
+  var REVIEW_SELECTOR = ".album-reviews.album-reviews .album-review-hero";
+  var ALBUM_LINK_SELECTOR = ".artwork";
+}
+
 var ARTWORK_DIV_SELECTOR = ".artwork";
 var SCORE_CIRCLE_SELECTOR = ".score-circle";
-
 
 document.querySelectorAll(REVIEW_SELECTOR).forEach((elem, i) => {
   var artDiv = elem.querySelector(ARTWORK_DIV_SELECTOR);
